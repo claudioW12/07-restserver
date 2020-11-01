aab8ed477b1d018ff447d473421feff4b64615e5
ClaudioW12 <cbarreraq2@gmail.com>
2020-11-01 19:32:31 -0300

    Restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,22 @@
-// ===========================
-// Puerto
-
+// ============================
+//  Puerto
+// ============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://ClaudioW12:pKZVEoCHe3Z6hzwi@cluster0.bznbb.mongodb.net/test';
+    //urlDB = process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,50 +1,26 @@
 require('./config/config');
 
 const express = require('express');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
-// parse application/x-www-form-urlencoded
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
 app.use(bodyParser.json())
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-})
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({ persona: body });
-
-    }
-
-})
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    let body = req.body;
-    res.json({
-        id: id,
-        body: body
-    });
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario');
-})
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
 
 app.listen(process.env.PORT, () => {
-
-    console.log('Escuchando el puerto 3000')
-
-})
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
