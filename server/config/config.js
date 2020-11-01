7585ad01b8e2b0f30e814482fc8d49cff1237545
ClaudioW12 <cbarreraq2@gmail.com>
2020-11-01 20:06:01 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ClaudioW12:pKZVEoCHe3Z6hzwi@cluster0.bznbb.mongodb.net/cafe';
-    //urlDB = process.env.MONGO_URI;
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
