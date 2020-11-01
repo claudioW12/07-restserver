a28cdf25bbac23aeaaa578d913348d53bbaf851d
ClaudioW12 <cbarreraq2@gmail.com>
2020-11-01 20:13:53 -0300

    Asignando operador ternario

--- server/config/config.js
+++ server/config/config.js
@@ -11,11 +11,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 //  Base de datos
 // ============================
-let urlDB;
+let urlDB = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
 
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
-} else {
-    urlDB = process.env.MONGO_URI;
-}
 process.env.URLDB = urlDB;
